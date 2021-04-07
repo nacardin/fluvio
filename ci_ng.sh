444007b7f7af41afc5aa12cc7e9d7a615a8ee135
Nick Cardin <nick@cardin.email>
2021-04-07 19:52:16 +0000

    WIP

---
+++ ci_ng.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -xe
+
+ssh-keygen -t RSA -f id_rsa
+
+aws ec2 import-key-pair --key-name fluvio-ci-$GITHUB_RUN_ID --public-key-material fileb://id_rsa.pub
+
+ec2_instance_id=$(aws ec2 run-instances \
+    --image-id $EC2_AMI \
+    --instance-type m5.2xlarge \
+    --security-group-ids sg-010f0a9ee7ddac50f \
+    --key-name fluvio-ci-$GITHUB_RUN_ID \
+    --query "Instances[0].InstanceId" \
+    --output text)
+
+echo ec2_instance_id
+echo EC2_INSTANCE_ID=$ec2_instance_id >> $GITHUB_ENV
+
+sleep 30
+
+ec2_instance_public_ip=$(aws ec2 describe-instances \
+    --instance-ids $ec2_instance_id \
+    --query "Reservations[0].Instances[0].PublicIpAddress" \
+    --output text)
+
+echo ec2_instance_public_ip
+
+ssh_opts=-o BatchMode=yes -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null
+ssh_remote=ubuntu@$(cat ec2_instance_public_ip)
+ssh_exec=ssh $SSH_REMOTE $SSH_OPTS
+
+$ssh_exec 'echo test'
+
+echo SSH_EXEC=$ssh_exec >> $GITHUB_ENV
