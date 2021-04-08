a2b4047041f3ae642d87d1997f6818880ce3f164
Nick Cardin <nick@cardin.email>
2021-04-08 03:10:00 +0000

    WIP

--- ci_ng.sh
+++ ci_ng.sh
@@ -31,7 +31,7 @@ ssh_remote="ubuntu@$ec2_instance_public_ip"
 ssh_exec="ssh -i ./id_rsa $ssh_remote $ssh_opts"
 
 echo SSH_EXEC_1=$ssh_exec >> $GITHUB_ENV
-echo SSH_EXEC=$ssh_exec export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/ubuntu/.cargo/bin;cd fluvio; >> $GITHUB_ENV
+echo SSH_EXEC="$ssh_exec export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/ubuntu/.cargo/bin;cd fluvio;" >> $GITHUB_ENV
 
 # export DEBIAN_FRONTEND=noninteractive
 # sudo apt-get install git -y -qq
