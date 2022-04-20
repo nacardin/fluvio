28c7a8a2e84ea61dbbbab6d77be357d12924d580
Nick Cardin <nick@cardin.email>
2022-04-20 12:31:16 -0400

    WIP

--- actions/zig-install.sh
+++ actions/zig-install.sh
@@ -6,7 +6,9 @@ echo "installing zig matrix.os=$MATRIX_OS"
 
 if [[ "$MATRIX_OS" == "ubuntu-latest" ]]; then
     echo "installing zig on ubuntu"
-    sudo snap install --beta --classic zig && \
+    tmp_dir=$(mktemp -d)
+    curl -S https://ziglang.org/download/0.9.1/zig-linux-x86_64-0.9.1.tar.xz | tar -xJ -C $tmp_dir
+    sudo install $tmp_dir/zig-linux-x86_64-0.9.1/zig /usr/local/bin
     sudo ${0%/*}/llvm.sh 13 && \
     echo "FLUVIO_BUILD_LLD=lld-13" | tee -a $GITHUB_ENV
 fi
